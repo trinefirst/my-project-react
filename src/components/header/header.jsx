89d71f9067eb80c62b9d5ed53581c281b8aca35c
trinefirst <trinefirst1@mail.ru>
2024-10-19 22:37:40 +0300

    text-upd

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,7 @@
 import './header.css'
 
 function Header () {
-    return <header className='header'>text</header>
+    return <header className='header'>im on the top of git</header>
 }
 
 export default Header;
